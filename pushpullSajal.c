0a75b7ca11bd9b4184a6b401f797c71d0c9d8e71
Sahoot <54327960+sajalrisal@users.noreply.github.com>
2019-09-12 13:06:43 -0500

    Creating pushpullSajal.c and

---
+++ pushpullSajal.c
@@ -0,0 +1,15 @@
+/************************************
+
+* Author: Sajal Risal
+* Date: 9/12/19
+
+* This is a test program to practice
+push/pull for github
+
+*************************************/
+#include <stdio.h>
+
+int main(){
+  printf("Sajal Risal\n");
+  return 0;
+}
